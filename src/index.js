4d1af07141477f4e166697b8e7e88bf9d3396d00
floren <florendg@gmail.com>
2021-11-25 20:43:08 +0100

    setup webpack project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+function component() {
+    const element = document.createElement('dev');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
+}
+
+document.body.appendChild(component());
