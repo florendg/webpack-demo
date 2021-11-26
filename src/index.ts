15a7fbaec7539c5bebba0a4731310572a86c2529
floren <florendg@gmail.com>
2021-11-26 18:03:17 +0100

    Setup Webpack for typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import _ from 'lodash';
+import _ from 'lodash'
+
 function component() {
-    const element = document.createElement('dev');
+    const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     return element;
 }
